483dc4f4163c2077ed61ccf7f40c5994eafb3e03
mpietrewicz <mpietrewicz@gmail.com>
2022-07-20 14:47:12 +0200

    Inicjacja projektu

---
+++ src/main/java/pl/altkom/mojedemo/MojeDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.altkom.mojedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/altkom/mojedemo/MojeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.altkom.mojedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
